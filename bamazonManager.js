4f4f1d87f55b07752b96b93073a2bbd30c9ca66e
Steve Travis <stevetravis11@gmail.com>
2019-05-16 18:16:39 -0400

    Add inventory function added, but it's not adding correct quantity.

--- bamazonManager.js
+++ bamazonManager.js
@@ -34,7 +34,7 @@ function start() {
   inquirer
     .prompt([
       {
-        name: "menuChoice",
+        name: "choice",
         type: "list",
         message: "Please select an option",
         choices: [
@@ -43,8 +43,7 @@ function start() {
           "Add to Inventory",
           "Add New Product",
           "Exit"
-        ],
-        name: "choice"
+        ]
       }
     ])
     .then(function(answer) {
@@ -135,7 +134,59 @@ function viewLowInv() {
 
 // function increases quantity of an item
 function addInv() {
-  console.log("Add inventory");
+  // prompt for product ID and quantity
+  inquirer
+    .prompt([
+      {
+        type: "input",
+        message: "ID # of item:",
+        name: "id",
+        validate: function(value) {
+          var valid = !isNaN(parseFloat(value));
+          return valid || "Please enter a number";
+        }
+      },
+      {
+        type: "input",
+        message: "Quantity to add?",
+        name: "quantity",
+        validate: function(value) {
+          var valid = !isNaN(parseFloat(value));
+          return valid || "Please enter a number";
+        }
+      }
+    ])
+    .then(function(item) {
+      db.query(
+        "SELECT * FROM products WHERE ?",
+        { item_id: item.id },
+        (err, results) => {
+          if (err) throw err;
+          let product = results[0];
+          console.log(`You selected ${product.product_name}`);
+          if (product.stock_quantity <= 0) {
+            console.log("Insufficient quantity");
+            start();
+          } else {
+            let newQuantity = product.stock_quantity + item.quantity;
+            db.query(
+              "UPDATE products SET ? WHERE ?",
+              [{ stock_quantity: newQuantity }, { item_id: item.id }],
+              function(err, results) {
+                if (err) throw err;
+                console.log("Inventory restock successful\n");
+                console.log(
+                  `There are now ${newQuantity} ${
+                    product.product_name
+                  }'s in the inventory.`
+                );
+                start();
+              }
+            );
+          }
+        }
+      );
+    });
 }
 
 // function adds a new product
